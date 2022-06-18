4d0e1d3ae86ac642fe0d9b6639f715165593d82a
jeremylombogia <jeremylombogia7@gmail.com>
2022-06-18 12:11:58 +0700

    fix: docker and more

--- stocksvc/internal/gorm/gorm_db.go
+++ stocksvc/internal/gorm/gorm_db.go
@@ -1,12 +1,20 @@
 package gorm
 
 import (
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"os"
 )
 
 func NewClient() (*gorm.DB, error) {
-	dsn := "root:12345678@tcp(127.0.0.1:3306)/stocksvc?charset=utf8mb4&parseTime=True&loc=Local"
+	host := os.Getenv("DB_HOST")
+
+	if host == "" {
+		host = "127.0.0.1"
+	}
+
+	dsn := fmt.Sprintf("root:12345678@tcp(%s:3306)/stocksvc?charset=utf8mb4&parseTime=True&loc=Local", host)
 
 	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
 }
